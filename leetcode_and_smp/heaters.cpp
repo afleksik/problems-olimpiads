ef1a32982250c18835ca91e34c8f3e602f844eb4
afleksik <romanenkov2006@gmail.com>
2023-10-06 20:57:54 +0300

    heaters medium task from leetcode added

---
+++ leetcode_and_smp/heaters.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int findRadius(vector<int>& houses, vector<int>& heaters) {
+    sort(houses.begin(),houses.end());
+    sort(heaters.begin(),heaters.end());
+    int n = houses.size();
+    int m = heaters.size();
+    int ans = 0;
+    for (auto h : houses) {
+        int id = lower_bound(heaters.begin(), heaters.end(), h) - heaters.begin();
+        int right = id >= m ? INT_MAX : heaters[id] - h;
+        int left = id - 1 < 0 ? INT_MAX : h - heaters[id - 1];
+        ans = max(ans, min(right, left));
+    }
+    return ans;
+}
